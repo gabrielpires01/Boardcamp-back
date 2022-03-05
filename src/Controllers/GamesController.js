74ef21f2c3ce224c73264777984d1af64f57087d
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-03-05 19:11:19 -0300

    Post Game Route Done

--- src/Controllers/GamesController.js
+++ src/Controllers/GamesController.js
@@ -9,4 +9,31 @@ const GetGames = async(req,res) => {
 	}
 }
 
-export { GetGames }
+const PostGames = async(req,res) => {
+	const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+
+	const newTotal = Number(stockTotal)
+	const newPrice = Number(pricePerDay)
+
+	if (newTotal <= 0 || newPrice <= 0) return res.sendStatus(400)
+
+	try {
+		const category = await db.query(`SELECT * FROM categories WHERE id = $1`,[categoryId]);
+		
+		if (!category.rows.length) return res.sendStatus(400)
+		const game = await db.query(`SELECT * FROM games WHERE name = $1`,[name])
+		
+		if (game.rows.length) return res.sendStatus(409)
+
+		await db.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay")
+						VALUES ($1, $2, $3, $4, $5)`, 
+						[name, image, newTotal, categoryId, newPrice]);
+
+		return res.sendStatus(201)
+
+	}catch(err) {
+		return res.status(500).send(err)
+	}
+}
+
+export { GetGames, PostGames }
--- src/Routes/GamesRoute.js
+++ src/Routes/GamesRoute.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { GetGames } from "../Controllers/GamesController.js";
+import { GetGames, PostGames } from "../Controllers/GamesController.js";
+import ValidateSchema from "../Middlewares/SchemaValidation.js";
+import gamesValidation from "../Schemas/GamesSchema.js";
 
 const gamesRoute = Router();
 
 gamesRoute.get('/games', GetGames);
+gamesRoute.post('/games', ValidateSchema(gamesValidation), PostGames)
 
 export default gamesRoute;
