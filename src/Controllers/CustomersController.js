cc78b95630fed2ed77672899d894ed880ad78b22
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-03-05 20:55:26 -0300

    Post Customers Done

--- src/Controllers/CustomersController.js
+++ src/Controllers/CustomersController.js
@@ -28,4 +28,19 @@ const GetOneCustomer = async(req,res) => {
 	}
 }
 
-export { GetCustomers, GetOneCustomer };
+const PostCustomers = async(req,res) => {
+	const { name, phone, cpf, birthday } = req.body;
+
+	try {
+		const customer = await db.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf])
+		if (customer.rowCount) return res.sendStatus(409)
+
+		await db.query(`INSERT INTO customers (name, phone, cpf, birthday)
+						VALUES ($1, $2, $3, $4)`, [name, phone, cpf, birthday])
+		return res.sendStatus(201)
+	}catch(err) {
+		return res.status(500).send(err)
+	}
+}
+
+export { GetCustomers, GetOneCustomer, PostCustomers };
--- src/Routes/CustomersRoute.js
+++ src/Routes/CustomersRoute.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { GetCustomers, GetOneCustomer } from "../Controllers/CustomersController.js";
+import { GetCustomers, GetOneCustomer, PostCustomers } from "../Controllers/CustomersController.js";
+import ValidateSchema from "../Middlewares/SchemaValidation.js";
+import customersValidation from "../Schemas/CustomersSchema.js";
 
 const customersRouter = Router()
 
 customersRouter.get('/customers', GetCustomers)
 customersRouter.get('/customers/:id', GetOneCustomer)
+customersRouter.post('/customers', ValidateSchema(customersValidation), PostCustomers)
 
 export default customersRouter;
