97fbf75eda1f3a482615e629d55bc1818ae3b86b
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-03-07 00:06:21 -0300

    Post Rentals Route

--- src/Controllers/RentalsController.js
+++ src/Controllers/RentalsController.js
@@ -14,16 +14,26 @@ const GetRentals = async(req,res) => {
 		const rentals = await db.query(`SELECT * FROM rentals ${customerId} ${gameId}`);
 
 		const rentalList = await Promise.all(rentals.rows.map(async(rental) => {
-			const customer = await db.query('SELECT * FROM customers WHERE id = $1', [rental.customerId])
-			const game = await db.query('SELECT * FROM games WHERE id = $1', [rental.gameId])
-
+			const customer = await db.query('SELECT * FROM customers WHERE id = $1', [rental.customerId]);
+			const game = await db.query('SELECT * FROM games WHERE id = $1', [rental.gameId]);
+			const category = await db.query('SELECT * FROM categories WHERE id = $1', [game.rows[0].categoryId]);
+			 
 			return {
 				...rental,
-				customer: {...customer},
-				game: {...game}
+				customer: {
+					id: customer.rows[0].id,
+					name: customer.rows[0].name
+				},
+				game: {
+					id: game.rows[0].id,
+					name: game.rows[0].name,
+					categoryId: game.rows[0].categoryId,
+					categoryName: category.rows[0].name
+				}
 			}
 		}));
 
+		
 		return res.send(rentalList)
 
 	}catch(err) {
@@ -31,4 +41,41 @@ const GetRentals = async(req,res) => {
 	}
 };
 
-export { GetRentals };
+const PostRentals = async(req,res) => {
+	const { customerId, gameId, daysRented } = req.body;
+
+	let rentDate = new Date();
+	rentDate = rentDate.toLocaleDateString("en-GB").split('T')[0];
+
+	
+	if (daysRented < 1) return res.sendStatus(400)
+
+	try {
+		const customer = await db.query('SELECT * FROM customers WHERE id = $1', [customerId])
+		if (!customer.rowCount) return res.sendStatus(400)
+
+		const game = await db.query('SELECT * FROM games WHERE id = $1', [gameId])
+		if (!game.rowCount) return res.sendStatus(400)
+
+		const originalPrice = Number(daysRented)*Number(game.rows[0].pricePerDay);
+
+		const rentals = await db.query(`SELECT * FROM rentals 
+										WHERE "gameId" = $1 AND "returnDate" IS NULL`, [game.rows[0].id,])
+
+		if(rentals.rowCount >= Number(game.rows[0].stockTotal) ) {
+			return res.sendStatus(400)
+		}
+
+		await db.query(`INSERT INTO rentals 
+							("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+						VALUES ($1, $2, $3, $4, $5, $6, $7)`, 
+						[customerId, gameId, rentDate, daysRented, null, originalPrice, null]);
+
+		return res.sendStatus(201)
+
+	}catch(err) {
+		return res.status(500).send(err)
+	}
+}
+
+export { GetRentals, PostRentals };
--- src/Routes/RentalsRoute.js
+++ src/Routes/RentalsRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { GetRentals } from "../Controllers/RentalsController.js";
+import { GetRentals, PostRentals } from "../Controllers/RentalsController.js";
 
 const rentalsRoute = Router();
 
 rentalsRoute.get('/rentals', GetRentals);
+rentalsRoute.post('/rentals', PostRentals);
 
 export default rentalsRoute
